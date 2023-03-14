78f316b804c8055f836652d3bf6079fd40eb422c
zrow <mrzrow@yandex.ru>
2023-03-14 15:30:16 +0300

    done

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <iterator>
+
+template <class RandomAccessIterator>
+void print(RandomAccessIterator b, RandomAccessIterator e) {
+    for (auto iter = b; iter != e; ++iter)
+        std::cout << *iter << ' ';
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> vec;
+    for (int i = 0; i < 10; ++ i)
+        vec.push_back(i * i);
+    print(vec.begin(), vec.end());
+    
+    std::deque<int> heap;
+    for (auto iter = vec.begin(); iter != vec.end(); ++iter)
+        heap.push_back(*iter);
+    print(heap.begin(), heap.end());
+
+    return 0;
+}
